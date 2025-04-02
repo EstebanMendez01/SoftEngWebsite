87856925814d9e9c2b1d78e6daa2806339b7d69c
EstebanMendez01 <mendez.esteban104@gmail.com>
2025-04-02 11:40:39 -0400

    Replace Vercel adapter with Netlify in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: vercel(),
+  adapter: netlify(),
 });
